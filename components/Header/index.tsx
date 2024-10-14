d09a32ef8b3977ef9bf35528c99ec4ca39aeb5b7
Alvin Ichi <nhatlapross@gmail.com>
2024-10-14 16:29:06 +0700

    update menu

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,7 +1,7 @@
 import ThreeDotIcon from "@/asset/icon/ThreeDotIcon";
-import { Image } from "@nextui-org/react";
+import { Button, Image } from "@nextui-org/react";
 import { useSession } from "next-auth/react";
-import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Avatar, User } from "@nextui-org/react";
+import { Dropdown, DropdownSection, DropdownTrigger, DropdownMenu, DropdownItem, Avatar, User } from "@nextui-org/react";
 import { useRouter } from "next/router";
 import { signOut } from 'next-auth/react';
 
@@ -15,7 +15,7 @@ export default function Header() {
 
     return (
         <div className="w-full flex justify-between items-center">
-            {/* <div className="flex items-center gap-3">
+            <div className="flex items-center gap-3">
                 <Image
                     isZoomed
                     width={60}
@@ -25,37 +25,35 @@ export default function Header() {
                 />
                 <span className="font-bold">{session?.user?.name}</span>
             </div>
-            <button>
-                <ThreeDotIcon />
-            </button> */}
-            <Dropdown placement="bottom-start">
+            <Dropdown className="bg-white text-primary"
+                classNames={{
+                    content: "p-0 border-divider-primary",
+                }}>
                 <DropdownTrigger>
-                    <div className="flex items-center gap-3">
-                        <Image
-                            isZoomed
-                            width={60}
-                            radius="lg"
-                            alt="NextUI Fruit Image with Zoom"
-                            src={session?.user?.image ?? "/path/to/default/image.jpg"}
-                        />
-                        <span className="font-bold">{session?.user?.name}</span>
-                    </div>
+                    <Button isIconOnly variant="light" startContent={<ThreeDotIcon />}>
+                    </Button>
                 </DropdownTrigger>
                 <DropdownMenu aria-label="User Actions" variant="flat">
-                    <DropdownItem key="profile" className="h-14 gap-2">
-                        <p className="font-bold">Signed in as</p>
-                        <p className="font-bold">{session?.user?.name}</p>
-                    </DropdownItem>
-                    <DropdownItem key="settings" onClick={() => router.push('/home')}>
-                        Home
-                    </DropdownItem>
-                    <DropdownItem key="team_settings" onClick={() => router.push('/profile')}>Profile</DropdownItem>
-                    <DropdownItem key="help_and_feedback" onClick={() => router.push('/policy')}>
-                        Term and Service
-                    </DropdownItem>
-                    <DropdownItem key="logout" color="danger" onClick={() => logOut()}>
-                        Log Out
-                    </DropdownItem>
+                    <DropdownSection aria-label="Preferences" showDivider>
+                        <DropdownItem key="profile" className="h-14 gap-2">
+                            <p className="font-bold">Signed in as</p>
+                            <p className="font-bold">{session?.user?.name}</p>
+                        </DropdownItem>
+                    </DropdownSection>
+                    <DropdownSection aria-label="Preferences" showDivider>
+                        <DropdownItem key="settings" onClick={() => router.push('/home')}>
+                            Home
+                        </DropdownItem>
+                        <DropdownItem key="team_settings" onClick={() => router.push('/profile')}>Profile</DropdownItem>
+                        <DropdownItem key="help_and_feedback" onClick={() => router.push('/policy')}>
+                            Term and Service
+                        </DropdownItem>
+                    </DropdownSection>
+                    <DropdownSection aria-label="Preferences" showDivider>
+                        <DropdownItem key="logout" color="danger" onClick={() => logOut()}>
+                            Log Out
+                        </DropdownItem>
+                    </DropdownSection>
                 </DropdownMenu>
             </Dropdown>
         </div>
